a6935dab788a2a97d37140b64684ee6f875e5bc1
jyoutetsu <kuangcaobaihuzi2017@gmail.com>
2021-10-09 16:26:26 +0900

    プロトタイプ これからプロトタイプを元に開発

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <v-app>
-    <v-main>
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import VMain from "./components/Main.vue";
+import VMain from '@/components/Main'
 
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
     VMain,
   },
-};
+}
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,37 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app> </v-navigation-drawer>
+  <v-app>
+    <v-app-bar color="purple" absolute dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-app-bar>
 
-    <v-main> </v-main>
+    <!-- 根据应用组件来调整你的内容 -->
+    <v-main>
+      <!-- 给应用提供合适的间距 -->
+      <common-body></common-body>
+    </v-main>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-text class="py-2 white--text text-center"> {{ new Date().getFullYear() }} — <strong>Vuetify</strong> </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import CommonBody from '@/components/CommonBody'
+
 export default {
-  data: () => ({ drawer: null }),
-};
+  components: {
+    CommonBody,
+  },
+}
 </script>
