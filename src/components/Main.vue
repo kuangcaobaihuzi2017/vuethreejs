c35db0b7058dc3bcd430e979983096e473afeaec
jyoutetsu <kuangcaobaihuzi2017@gmail.com>
2021-10-09 16:35:10 +0900

    改了下页脚

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -20,7 +20,7 @@
     </v-main>
     <v-footer dark padless>
       <v-card class="flex" flat tile>
-        <v-card-text class="py-2 white--text text-center"> {{ new Date().getFullYear() }} — <strong>Vuetify</strong> </v-card-text>
+        <v-card-text class="py-4 purple white--text text-center"> {{ new Date().getFullYear() }} — <strong>Vuetify</strong> </v-card-text>
       </v-card>
     </v-footer>
   </v-app>
