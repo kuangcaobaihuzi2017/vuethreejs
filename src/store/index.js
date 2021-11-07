60259d16b76ff5eb869a13978efa045d19cd22f0
unknown <kuangcaobaihuzi2017@gmail.com>
2021-11-07 19:05:09 +0900

    当画面初始化渲染完成后，用鼠标操纵相机后，莫名其妙相机聚焦低了一点，这是因为lookat没有设置

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,7 @@ export default new Vuex.Store({
     // 是否渲染页面判定
     updatePageFlag: true,
     // lookup位置
-    lookAtPosition: null,
+    lookAtPosition: new THREE.Vector3(0, 1, 0),
     pageInfo: [
       { pageName: 'topage', flag: true },
       { pageName: 'serviceList', flag: false },
@@ -36,18 +36,22 @@ export default new Vuex.Store({
       var pageTarget = ''
       // 将其他页面的聚焦设定为false
       for (var i = 0; i < state.pageInfo.length; i++) {
+        // 获取当前页面所在位置
         if (state.pageInfo[i].flag) {
           pageTarget = state.pageInfo[i].pageName
         }
+        // 将其他页面的聚焦设定为false
         state.pageInfo[i].flag = false
         console.log('pageTarget : ' + pageTarget)
       }
       // 将页面聚焦设定到服务一览
       state.pageInfo[1].flag = true
-      state.positionDto
+      // state.positionDto
     },
     calLookAtPosition(state) {
       if (state.pageInfo[0].flag) {
+        state.lookAtPosition = new THREE.Vector3(0, 1, 0)
+      } else if (state.pageInfo[1].flag) {
         state.lookAtPosition = new THREE.Vector3(0, 1, 0)
       }
     },
