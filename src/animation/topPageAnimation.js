17b6340240372cff9393197e69982fe9377ccd24
unknown <kuangcaobaihuzi2017@gmail.com>
2021-11-23 13:54:49 +0900

    完善了艺术字

--- src/animation/topPageAnimation.js
+++ src/animation/topPageAnimation.js
@@ -3,7 +3,7 @@ function changeFontPosition() {
   // 如果字体的加载已经完成
   if (store.state.scene.getObjectByName('fontFront') !== undefined) {
     // 如果已经移动到指定位置，则直接返回
-    if (store.state.scene.getObjectByName('fontFront').position.z < 2.5) {
+    if (store.state.scene.getObjectByName('fontFront').position.z < 3) {
       return
     } else {
       console.log('fontFront : ' + store.state.scene.getObjectByName('fontFront').position.z)
