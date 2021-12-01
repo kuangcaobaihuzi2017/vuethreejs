abbebdb95b46e5b0df0fec35cdc068cf1fceb30c
unknown <kuangcaobaihuzi2017@gmail.com>
2021-12-01 23:39:37 +0900

    追加了公司访问页面

--- src/pageinfo/scene.js
+++ src/pageinfo/scene.js
@@ -3,7 +3,7 @@ import store from '../store'
 function createScene() {
   var scene = new THREE.Scene()
   scene.background = new THREE.Color(0xf0f0f0)
-  scene.fog = new THREE.Fog(0xf0f0f0, 2, 30)
+  // scene.fog = new THREE.Fog(0xf0f0f0, 2, 30)
   // 将创建好的scene提交到store
   store.commit('createScene', scene)
 }
